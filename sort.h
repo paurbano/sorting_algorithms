7fcf1dc3533b3c4b6c2f321e10bbdeb45ffa519b
paurbano <paurbano@gmail.com>
2020-02-02 20:28:16 +0000

    header file

---
+++ sort.h
@@ -0,0 +1,87 @@
+#ifndef SORTING
+#define SORTING
+#include "sort.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
+/**
+ * enum kind_e - Cards
+ *
+ * @SPADE: for spade cards
+ * @HEART: for heart cards
+ * @CLUB: for club cards
+ * @DIAMOND: for diamond cards
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void swap_int(int *a, int *b);
+void swap_sort(listint_t **list, listint_t *ptr, listint_t *tmp);
+int _strlen(char *str);
+int create_file(const char *filename, char *text_content);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+#endif
