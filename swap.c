bb02933c58f2960eccb8809824fefd7ee55d66f6
paurbano <paurbano@gmail.com>
2020-02-02 20:33:38 +0000

    support functions

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+/**
+ * swap_int - swap two numbers
+ * @a: firts number
+ * @b: second number
+ *
+ * Description: Write a function that swaps the values of two integers
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
