bc08d07cde355e61121eb685b1d3d1da44ddf90d
paurbano <paurbano@gmail.com>
2020-02-03 18:32:45 +0000

    fix first validation

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,38 @@
 #include "sort.h"
-
 /**
  * insertion_sort_list - sorted a list using the Insertion sort algorithm
  *
  * @list: The list to be sorted
  * Return: nothing
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *mark;
+	listint_t *current, *guia;
 
-	if ((*list)->next == NULL || list == NULL || (*list) == NULL)
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
 		return;
-
-	temp = *list;
-
-	/* marker point in the array */
-	mark = *list;
-
-	while (mark != NULL)
+	current = *list;
+	/* guia point in the array */
+	guia = *list;
+	while (guia != NULL)
 	{
-		mark = mark->next;
-		/* checks compare from the marker to the start */
-		while (temp->prev && (temp->n < temp->prev->n))
+		guia = guia->next;
+		/* checks compare from the guia to the start */
+		while (current->prev && (current->n < current->prev->n))
 		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
 
-			temp->next = temp->prev;
-			temp->prev = temp->next->prev;
-			if (temp->prev != NULL)
-				temp->prev->next = temp;
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			if (current->prev != NULL)
+				current->prev->next = current;
 			else
-				*list = temp;
-			temp->next->prev = temp;
+				*list = current;
+			current->next->prev = current;
 			print_list(*list);
 		}
-		temp = mark;
+		current = guia;
 	}
 }
